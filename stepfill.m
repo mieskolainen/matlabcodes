eb85dab03a804078a8361778a3028d55d8e852c9
Mikael Mieskolainen <mikael.mieskolainen@cern.ch>
2017-12-30 16:36:03 +0200

    Add files via upload

---
+++ stepfill.m
@@ -0,0 +1,42 @@
+% Stephist style filled plot
+%
+% input:      x = x-axis BIN CENTER (N)
+%             u = upper curve data (N)
+%             l = lower curve data (N)
+%         color = Fill color
+%          edge = Edge color
+%  transparency = Fill tranparency [0...1]
+%
+% mikael.mieskolainen@cern.ch, 2017
+
+function stepfill(x,u,l,color,edge,transparency)
+
+if (length(u) ~= length(l))
+    error('stepfill:: length(u) ~= length(l) !');
+end
+if (length(u) ~= length(x))
+    error('stepfill:: x,u,l, input dimensions do not match !');
+end
+
+x_ = [];
+u_ = [];
+l_ = [];
+
+% Create steps
+for i = 1:length(x)
+    if (i < length(x))
+        delta = (x(i+1) - x(i))/2;
+    end
+    x_(end+1) = x(i) - delta;
+    x_(end+1) = x(i) + delta;
+    u_(end+1) = u(i);
+    u_(end+1) = u(i);
+    l_(end+1) = l(i);
+    l_(end+1) = l(i);
+end
+
+% Plot each bar separatily, for robustness (sometimes fails otherwise for noisy data)
+for i = 1:2:length(x_)
+    plotfill(x_(i:i+1),u_(i:i+1),l_(i:i+1),color,edge,transparency); hold on;
+end
+end
---
+++ stephist.m
@@ -0,0 +1,22 @@
+% Stephist with CENTER bins
+%
+% INPUT: x = Bin center  (N)
+%        n = Bin content (N)
+%
+% One can use together with hist(data, bincenters)
+%
+% mikael.mieskolainen@cern.ch, 2017
+
+function h = stephist(x, n, varargin)
+
+x = x(:)';
+n = n(:)';
+
+if (length(n) ~= length(x))
+    error('stephist:: x,n input dimensions do not match !');
+end
+
+h = stairs([x(1)-(x(2)-x(1))/2 ...
+            x-(x(2)-x(1))/2 ...
+            x(length(x))+(x(2)-x(1))/2], [0 n 0], varargin{:});
+end
